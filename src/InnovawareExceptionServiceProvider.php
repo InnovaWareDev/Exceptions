2ed72454f3f8448416076b24655b9ba6fb1f0111
kareldeij <k.deij@outlook.com>
2024-05-15 10:50:37 +0200

    Fix errors

--- src/InnovawareExceptionServiceProvider.php
+++ src/InnovawareExceptionServiceProvider.php
@@ -7,7 +7,7 @@ use Illuminate\Support\ServiceProvider;
 class InnovawareExceptionServiceProvider extends ServiceProvider {
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__.'/resources/views', 'exceptions');
+        $this->loadViewsFrom(__DIR__.'/resources/views', 'emails');
 
     }
     public function register()
