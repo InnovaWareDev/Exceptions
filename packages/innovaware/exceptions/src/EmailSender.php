399ea7418105ac130fd68d9748ecf86977182d99
kareldeij <k.deij@outlook.com>
2024-05-15 09:39:43 +0200

    Initial commit

---
+++ packages/innovaware/exceptions/src/EmailSender.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Innovaware\Exceptions;
+
+use Illuminate\Support\Facades\Mail;
+
+class EmailSender
+{
+    public static function sendErrorEmail($error)
+    {
+        Mail::send('emails.error', ['error' => $error], function ($message) {
+            $message->to('support@innovaware.nl')->subject('Foutmelding');
+        });
+    }
+}
