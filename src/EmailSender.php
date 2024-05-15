f3b3a27d85dfa159eb8e787fc3003082b6e015db
kareldeij <k.deij@outlook.com>
2024-05-15 17:00:54 +0200

    Fix errors

--- src/EmailSender.php
+++ src/EmailSender.php
@@ -2,6 +2,7 @@
 
 namespace Innovaware\Exceptions;
 
+use Illuminate\Http\Client\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Mail;
 
@@ -16,6 +17,23 @@ class EmailSender
             "code" => $error->getCode(),
             "message" => $error->getMessage(),
             "trace" => $error->getTraceAsString(),
+            "ip" => request()->ip(),
+            "browser" => request()->userAgent(),
+            "route" => request()->route(),
+            "url" => request()->url(),
+            "has_session" => request()->hasSession(),
+            "user" => auth()->user(),
+            "method" => \request()->method(),
+            "accept_content_type" => request()->header('Accept'),
+            "language_preference" => request()->header('Accept-Language'),
+            "query_parameters" => request()->query(),
+            "cookies" => request()->cookie(),
+            "uploaded_file" => request()->file('upload'),
+            "session_data" => request()->session()->all(),
+            "json_data" => request()->json()->all(),
+            "request_headers" => request()->header(),
+            "request_parameters" => request()->input(),
+            "server_environment" => $_SERVER,
             "APP_ENV" => env('APP_ENV'),
         ];
 
