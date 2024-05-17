e63e3d47754bbb16eccc28f0009809d781353f3c
kareldeij <k.deij@outlook.com>
2024-05-17 12:34:07 +0200

    Fix errors

--- src/EmailSender.php
+++ src/EmailSender.php
@@ -12,39 +12,37 @@ class EmailSender
     {
         $app_key = env('INNOVAWARE_APP_KEY');
         if ($app_key != null) {
-            $errorObj = [
-                "file" => $error->getFile(),
-                "line" => $error->getLine(),
-                "code" => $error->getCode(),
-                "message" => $error->getMessage(),
-                "trace" => $error->getTraceAsString(),
-                "ip" => request()->ip(),
-                "browser" => request()->userAgent(),
-                "route" => request()->route(),
-                "url" => request()->url(),
-                "has_session" => request()->hasSession(),
-                "user" => auth()->user(),
-                "method" => \request()->method(),
-                "accept_content_type" => request()->header('Accept'),
-                "language_preference" => request()->header('Accept-Language'),
-                "query_parameters" => request()->query(),
-                "cookies" => request()->cookie(),
-                "uploaded_file" => request()->file('upload'),
-                "session_data" => request()->session()->all(),
-                "json_data" => request()->json()->all(),
-                "request_headers" => request()->header(),
-                "request_parameters" => request()->input(),
-                "server_environment" => $_SERVER,
-                "APP_ENV" => env('APP_ENV'),
-            ];
 
-            $promise = Http::async()->post("https://portaal.innovaware.nl/webhooks/app/$app_key/exception", json_encode($errorObj))
-                ->then(function ($response) {
-                });
+            $url = "https://portaal.innovaware.nl";
 
-            $promise->wait();
+            Http::post("$url/webhooks/app/$app_key/exception",
+                [
+                    "file" => $error->getFile(),
+                    "line" => $error->getLine(),
+                    "code" => $error->getCode(),
+                    "message" => $error->getMessage(),
+                    "trace" => $error->getTraceAsString(),
+                    "ip" => request()->ip(),
+                    "browser" => request()->userAgent(),
+                    "route" => request()->route(),
+                    "url" => request()->url(),
+                    "has_session" => request()->hasSession(),
+                    "user" => auth()->user(),
+                    "method" => \request()->method(),
+                    "accept_content_type" => request()->header('Accept'),
+                    "language_preference" => request()->header('Accept-Language'),
+                    "query_parameters" => request()->query(),
+                    "cookies" => request()->cookie(),
+                    "uploaded_file" => request()->file('upload'),
+                    "session_data" => request()->session()->all(),
+                    "json_data" => request()->json()->all(),
+                    "request_headers" => request()->header(),
+                    "request_parameters" => request()->input(),
+                    "server_environment" => $_SERVER,
+                    "APP_ENV" => env('APP_ENV'),
+                ]
+            );
         }
-
     }
 
 }
